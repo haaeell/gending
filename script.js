0ed0608c59ed668228474b0b3bd4b5a5d96ec348
hael <haikaljawahir434@gmail.com>
2023-08-27 19:37:16 +0700

    Scroll on top

--- script.js
+++ script.js
@@ -47,3 +47,23 @@
           prevEl: ".swiper-button-prev",
         },
       });
+
+      document.addEventListener("DOMContentLoaded", function () {
+        const scrollToTopButton = document.getElementById("scrollToTop");
+      
+        window.addEventListener("scroll", function () {
+          if (window.scrollY > 200) {
+            scrollToTopButton.classList.add("show-scroll");
+          } else {
+            scrollToTopButton.classList.remove("show-scroll");
+          }
+        });
+      
+        scrollToTopButton.addEventListener("click", function () {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+        });
+      });
+      
